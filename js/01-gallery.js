1a0acd5b6a6fb01108a498730023f0b67646efae
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2021-11-06 20:45:42 +0200

    add task 2 and fix task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-//1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// 2.Реализация делегирования на div.gallery и получение url большого изображения.
-// 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN 
-// сервис jsdelivr и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
-// 4.Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// const instance = basicLightbox.create(`<img src='' width="800" height="600">`)
+let currentIndex = 0;
 const galleryRef = document.querySelector('.gallery');
 const createGalleryItemsMarkup = galleryItems.map(
     ({ preview, original, description }) =>
@@ -22,7 +16,6 @@ const createGalleryItemsMarkup = galleryItems.map(
 </div>`
 ).join('');
 galleryRef.insertAdjacentHTML('beforeend', createGalleryItemsMarkup);
-
 const instance = basicLightbox.create(
     `<img src="" width="800" height="600">`,
     {
@@ -46,58 +39,65 @@ function getImageUrl(event) {
         return
     };
     
-    const imageUrl = event.target.dataset.source;
-    let srclink = instance.element().querySelector('img');
-    srclink.setAttribute("src", imageUrl);
+   let srcLink = event.target.dataset.source;
+    setOriginalURL(srcLink);
+    // currentIndex = Number(event.target.dataset.index);
     instance.show();
+    // console.log(srcLink);
 };
 
-function onEscPress({ key }) {
-    switch (key) {
-        case "ArrowRight":
-            currentIndex += 1
-        if (currentIndex >= galleryItems.length) {
-            currentIndex = 0
-        }
-        setOriginalURL(galleryItems[currentIndex].original)
-            break;
-        case "ArrowLeft":
-            currentIndex -= 1
-            if (currentIndex <= 0) {
-                currentIndex = galleryItems.length - 1
-            }
-            setOriginalURL(galleryItems[currentIndex].original)
-            break;
-        case "Escape":
-            instance.close();
-            break;
-        
-        default:
-            break;
-    }
+
+function setOriginalURL(url) {
+    instance.element().querySelector("img").src = url
 }
-    
-// Второй вариант
-    // function onEscPress (evt) {
-//     if (evt.key === "Escape") {
-//         instance.close();
-//     };
-//     if (evt.key === "ArrowRight") {
-//         currentIndex += 1
+
+
+// первый вариант
+// function onEscPress({ key }) {
+//     switch (key) {
+//         case "ArrowRight":
+//             currentIndex += 1
 //         if (currentIndex >= galleryItems.length) {
 //             currentIndex = 0
 //         }
-//         instance.element().querySelector("img").src = galleryItems[currentIndex].original
-//         console.log("right");
-//     };
-//     if (evt.key === "ArrowLeft") {
-//         currentIndex -= 1
-//         if (currentIndex <= 0) {
-//             currentIndex = galleryItems.length-1
-//         }
-//         instance.element().querySelector("img").src = galleryItems[currentIndex].original
-//         console.log("Left");
+//         setOriginalURL(galleryItems[currentIndex].original)
+//             break;
+//         case "ArrowLeft":
+//             currentIndex -= 1
+//             if (currentIndex <= 0) {
+//                 currentIndex = galleryItems.length - 1
+//             }
+//             setOriginalURL(galleryItems[currentIndex].original)
+//             break;
+//         case "Escape":
+//             instance.close();
+//             break;
+        
+//         default:
+//             break;
 //     }
-//     }
-console.log(galleryItems);
+// }
 
+// Второй вариант
+    function onEscPress (evt) {
+    if (evt.key === "Escape") {
+        instance.close();
+    };
+    if (evt.key === "ArrowRight") {
+        currentIndex += 1
+        if (currentIndex >= galleryItems.length) {
+            currentIndex = 0
+        }
+        instance.element().querySelector("img").src = galleryItems[currentIndex].original
+        console.log("right");
+    };
+    if (evt.key === "ArrowLeft") {
+        currentIndex -= 1
+        if (currentIndex <= 0) {
+            currentIndex = galleryItems.length-1
+        }
+        instance.element().querySelector("img").src = galleryItems[currentIndex].original
+        console.log("Left");
+    }
+    }
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const createGalleryItemsMarkup = galleryItems.map(
+    ({ preview, original, description }) =>
+        `<li>
+        <a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}" />
+        </a>
+        </li>`
+).join('');
+galleryRef.insertAdjacentHTML('beforeend', createGalleryItemsMarkup)
 
-console.log(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+    captionDelay: 250,
+  disableScroll: true,
+});
+// console.log(galleryItems);
